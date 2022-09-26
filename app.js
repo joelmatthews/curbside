5eac9e4f1ef10f37a5112584e64835c00f3407a1
Joel Matthews <joelmatthews.A@gmail.com>
2022-09-25 21:44:20 -0400

    ready to deploy

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const ExpressError = require('./utilities/ExpressError');
 const session = require('express-session');
 const flash = require('connect-flash');
 const ejsMate = require('ejs-mate');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -17,24 +20,40 @@ const User = require('./models/user');
 
 const app = express();
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/curbside';
+
+// 'mongodb://localhost:27017/curbside'
+
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/curbside');
+    await mongoose.connect(dbUrl);
 }
 
 //Serve Static Assets, form data from body, and method override
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(mongoSanitize());
+
+const secret = process.env.SECRET || 'thisisabadsecret';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
 
 const sessionConfig = {
     name: 'session',
-    secret: 'thisisabadsecret',
+    secret: process.env.SECRET || 'thisisabadsecret',
     saveUninitialized: true,
     resave: false,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7, 
         maxAge: 1000 * 60 * 60 * 24 * 7
 
@@ -50,6 +69,57 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dubud8fgb/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dubud8fgb/",
+    "https://fonts.gstatic.com/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+    "https://res.cloudinary.com/dubud8fgb/"
+];
+const fontSrcUrls = [ "https://res.cloudinary.com/dubud8fgb/", "https://fonts.gstatic.com/" ];
+ 
+app.use(
+    helmet.contentSecurityPolicy({
+        directives : {
+            defaultSrc : [],
+            connectSrc : [ "'self'", ...connectSrcUrls ],
+            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+            workerSrc  : [ "'self'", "blob:" ],
+            objectSrc  : [],
+            imgSrc     : [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dubud8fgb/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                "https://images.unsplash.com/"
+            ],
+            fontSrc    : [ "'self'", ...fontSrcUrls ],
+            mediaSrc   : [ "https://res.cloudinary.com/dubud8fgb/" ],
+            childSrc   : [ "blob:" ]
+        }
+    })
+);
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
@@ -74,7 +144,7 @@ app.use('/items/:id/comment', commentRoutes);
 app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render('home');
 });
 
 app.all('*', (req, res, next) => {
--- validation.js
+++ validation.js
@@ -1,17 +1,40 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAtributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.itemJoiSchema = Joi.object({
     item: Joi.object({
-        name: Joi.string().required(),
+        name: Joi.string().required().escapeHTML(),
         location: Joi.object({
-            addressLineOne: Joi.string().required(),
-            addressLineTwo: Joi.string().optional().allow(''),
-            city: Joi.string().required(),
-            state: Joi.string().required(),
+            addressLineOne: Joi.string().required().escapeHTML(),
+            addressLineTwo: Joi.string().optional().allow('').escapeHTML(),
+            city: Joi.string().required().escapeHTML(),
+            state: Joi.string().required().escapeHTML(),
             zip: Joi.number().required()
         }),
         category: Joi.string().valid('furniture', 'clothing', 'toys', 'entertainment', 'other').required(),
-        details: Joi.string().max(500).required(),
+        details: Joi.string().max(500).required().escapeHTML(),
         image: Joi.string().optional()
 
     })
